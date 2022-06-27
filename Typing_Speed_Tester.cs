1e80fefcb06dc486cb9fde0ee51cc706d876769f
Kooreis <kooreis.official@gmail.com>
2022-06-27 15:00:00 -0500

    In this update, the necessary libraries are imported and the main program class is defined. An array of words is also initialized.

--- Typing_Speed_Tester.cs
+++ Typing_Speed_Tester.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 using System.Diagnostics;
 using System.Linq;
@@ -8,21 +7,5 @@ class Program
     static void Main(string[] args)
     {
         string[] words = { "the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
-        string sentence = string.Join(" ", words);
-        Console.WriteLine("Type the following: \n" + sentence);
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
-        string input = Console.ReadLine();
-        stopwatch.Stop();
-        if (input == sentence)
-        {
-            double typingSpeed = (double)words.Length / stopwatch.Elapsed.TotalMinutes;
-            Console.WriteLine("Your typing speed is " + typingSpeed + " words per minute");
-        }
-        else
-        {
-            Console.WriteLine("Incorrect input. Please try again.");
-        }
     }
 }
-```
