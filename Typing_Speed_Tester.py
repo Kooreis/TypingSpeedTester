a4f8a0465adbe1fb4250b0ce8737733a39562c32
Kooreis <kooreis.official@gmail.com>
2022-06-28 16:00:00 -0500

    Defined a function to calculate typing speed. It takes the input text and start and end times as arguments, calculates the number of words per minute, and returns the typing speed.

--- Typing_Speed_Tester.py
+++ Typing_Speed_Tester.py
@@ -1,1 +1,5 @@
-import time
+def typing_speed(input_text, start_time, end_time):
+    words_per_minute = len(input_text.split()) / 5.0
+    time_diff = end_time - start_time
+    speed = words_per_minute / time_diff * 60
+    return speed
