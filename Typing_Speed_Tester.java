7aec311551edbca74addfae9165b76e30f69038b
Kooreis <kooreis.official@gmail.com>
2022-06-28 16:00:00 -0500

    Defines the TypingSpeedTester class and the main method. Initializes the Scanner for user input and the test string. Prompts the user to type the test string.

--- Typing_Speed_Tester.java
+++ Typing_Speed_Tester.java
@@ -1,3 +1,5 @@
-import java.util.Scanner;
-import java.time.Duration;
-import java.time.Instant;
+public class TypingSpeedTester {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String testString = "The quick brown fox jumps over the lazy dog";
+        System.out.println("Type the following: '" + testString + "'");
